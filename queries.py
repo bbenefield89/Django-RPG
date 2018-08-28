9cd3c9e003841f87c5f82ecc40f3bdee5de06c4b
bbenefield89 <bsquared18@gmail.com>
2018-08-28 17:02:22 -0400

    RETURN WEAPON & NOT WEAPON AMOUNTS
    - in queries.py

--- queries.py
+++ queries.py
@@ -10,3 +10,9 @@ char_amount = Character.objects.all().count()
  # return total amount of items in DB
  ##
 items_amount = Item.objects.all().count() + Weapon.objects.all().count()
+
+###
+ # return how many items are weapons and how many are not
+ ##
+weapon_amount = Weapon.objects.all().count()
+not_weapon_amount = Item.objects.all().count()
