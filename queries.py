2c8c2e205672810de87bd362370c09ec39623c1e
bbenefield89 <bsquared18@gmail.com>
2018-08-28 16:57:53 -0400

    ADD GET ITEM AMOUNT QUERY
    - in queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,12 @@
 from charactercreator.models import Character
+from armory.models import Item
 
 ###
  # return amount of total characters in DB
  ##
 char_amount = Character.objects.all().count()
+
+###
+ # return total amount of items in DB
+ ##
+items_amount = Item.objects.all().count()
