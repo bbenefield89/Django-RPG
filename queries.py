07b1926e5e0db2b14998cb91d9ef68a00f930061
bbenefield89 <bsquared18@gmail.com>
2018-08-28 17:00:54 -0400

    FIX ITEM COUNT
    - in queries.py

--- queries.py
+++ queries.py
@@ -1,5 +1,5 @@
 from charactercreator.models import Character
-from armory.models import Item
+from armory.models import Item, Weapon
 
 ###
  # return amount of total characters in DB
@@ -9,4 +9,4 @@ char_amount = Character.objects.all().count()
 ###
  # return total amount of items in DB
  ##
-items_amount = Item.objects.all().count()
+items_amount = Item.objects.all().count() + Weapon.objects.all().count()
